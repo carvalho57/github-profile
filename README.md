857f9ac3eb59e56d744245b237011ab545273cd5
carvalho57 <gabrielfeocarvalho57@gmail.com>
2024-03-06 19:55:16 -0300

    feat: realiza busca das informações do usuário após clicar no botão Search

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ In this project you create a search app that uses GitHub API to retrieve user in
 
 ## User Stories
 - [x] User can enter a username
-- [ ] User can click on search button to retrieve information
-- [ ] User can see the avatar, username, followers and repository count of searched user
+- [x] User can click on search button to retrieve information
+- [x] User can see the avatar, username, followers and repository count of searched user
 - [ ] User can see the top 4 repositories of searched user
 - [ ] User should get an alert if the username is not valid
 
@@ -24,3 +24,7 @@ To get the data you need to communicate with GitHub API. you can either
 * [Read Docs](https://docs.github.com/en/rest)
 * [Check API directly](https://api.github.com/users/chaharshivam)
 * [Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API)
+
+## Outros links
+* https://docs.github.com/en/rest/using-the-rest-api/getting-started-with-the-rest-api?apiVersion=2022-11-28
+* https://docs.github.com/en/rest/users/users?apiVersion=2022-11-28#get-a-user
--- main.js
+++ main.js
@@ -1,18 +1,83 @@
 
 const submitButton = document.querySelector('.search__button');
+const submitInput = document.querySelector('.search__input');
+const usersContainer = document.querySelector('.users__container');
 
-submitButton.addEventListener('click', function(e) {
+submitButton.addEventListener('click', function (e) {
     e.preventDefault();
     e.stopPropagation();
 
-    alert('Click');
+    const username = submitInput.value;
+
+    if (username === '') {
+        alert('Informe um username válido');
+    }
+
+    getUserInfo(username);
 
 });
 
-//Buscar as informações do perfil
+function getUserInfo(username) {
+
+    fetch(`https://api.github.com/users/${username}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Não foi possível localizar o usuário');
+            }
+
+            return response;
+        })
+        .then(response => response.json())
+        .then(data => {
+
+            const user = {
+                name: data.name ?? data.login,
+                username: data.login,
+                followers: data.followers,
+                repositoriesCount: data.public_repos,
+                avatarURL: data.avatar_url
+            };
+
+            createCard(user);
+
+        }).catch(error => {
+            dialog(error.message);
+        });
+}
+
+
+function createCard(user) {
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const img = document.createElement('img');
+    img.setAttribute('title', 'User image profile');
+    img.src = user.avatarURL;
+
+    card.appendChild(img);
+
+    const userInfo = document.createElement('div');
+    userInfo.classList.add('card__user__info');
+
+    userInfo.innerHTML = `
+            <p>Name: ${user.name}</p>
+            <p>Username: ${user.username}</p>
+            <p>Followers: ${user.followers}</p>
+            <p>Repositories count: ${user.repositoriesCount}</p>
+        `;
+
+    card.appendChild(userInfo);
+
+    usersContainer.appendChild(card);
+
+}
+
+function dialog(message) {
+    alert(message);
+}
 
 //Renderizar informações do perfil
 
 // Toggle themas
 // Armazenar em cookies ou localStorage as preferências
-    
